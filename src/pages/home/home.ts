3ee00631f1be6c0b00081bb6bc5d02ab67c5c8b3
didin jamaludin <didinj@gmail.com>
2017-07-21 06:24:03 +0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LinkedIn, LinkedInLoginScopes } from '@ionic-native/linkedin';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,61 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  scopes: LinkedInLoginScopes[] = ['r_basicprofile', 'r_emailaddress', 'rw_company_admin', 'w_share'];
+  isLoggedIn: boolean = false;
+  selfData = { id:"", firstName:"", lastName:"" };
 
+  constructor(public navCtrl: NavController, private linkedin: LinkedIn) {}
+
+  ionViewDidAppear() {
+    this.linkedin.hasActiveSession().then((active) => {
+      this.isLoggedIn = active;
+      if(this.isLoggedIn === true) {
+        this.getSelfData();
+      }
+    });
   }
 
+  login() {
+    this.linkedin.login(this.scopes, true)
+      .then(() => {
+        this.isLoggedIn = true;
+        this.getSelfData();
+      })
+      .catch(e => console.log('Error logging in', e));
+  }
+
+  logout() {
+    this.linkedin.logout();
+    this.isLoggedIn = false;
+    console.log(this.isLoggedIn);
+  }
+
+  getSelfData() {
+    this.linkedin.getRequest('people/~')
+      .then(res => {
+        this.selfData = res;
+        //this.openProfile(res.id);
+      })
+      .catch(e => console.log(e));
+  }
+
+  openProfile(memberId) {
+    this.linkedin.openProfile(memberId)
+      .then(res => console.log(res))
+      .catch(e => console.log(e));
+  }
+
+  shareSomething() {
+    const body = {
+      comment: 'May I Share something on my profile?',
+      visibility: {
+        code: 'anyone'
+      }
+    };
+
+    this.linkedin.postRequest('~/shares', body)
+      .then(res => console.log(res))
+      .catch(e => console.log(e));
+  }
 }
